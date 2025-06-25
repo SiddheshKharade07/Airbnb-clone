b676f6b44543509fc2979dcdbb451af88da6d853
Siddhesh Kharade <hnkharade@gmail.com>
2025-06-25 19:14:26 +0530

    initial commits

---
+++ routes/review.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+const reviewController = require("../controllers/reviews");
+
+// Reviews
+// Post Review Route(Creates new Review)
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+
+// Delete Review Route
+router.delete("/:reviewId", isReviewAuthor, wrapAsync(reviewController.destroyReview));
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
+const userController = require("../controllers/users");
+
+// Signup route
+router.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup));
+
+// Login route
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl, passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), userController.login);
+
+// Logout route
+router.get("/logout", userController.logout);
+
+module.exports = router;
