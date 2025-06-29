b4ca08d50e00953b83f8612333c41ccaaa08d6f5
Siddhesh Kharade <hnkharade@gmail.com>
2025-06-29 15:52:46 +0530

    Modularize filter routes using dynamic category handling

---
+++ utils/filterCategories.js
@@ -0,0 +1,14 @@
+// utils/filterCategories.js
+module.exports = [
+  "trending",
+  "rooms",
+  "mountains",
+  "farms",
+  "arctics",
+  "beaches",
+  "cities",
+  "pools",
+  "campings",
+  "domes",
+  "boats",
+];
