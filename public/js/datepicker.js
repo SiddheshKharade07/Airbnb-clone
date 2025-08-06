047561ea6bb3958ee0e60facf28c659a54ee4fad
Siddhesh Kharade <hnkharade@gmail.com>
2025-08-06 15:25:12 +0530

    makes minimal changes in show page

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    flatpickr("#checkin", {
-      dateFormat: "Y-m-d",
-      minDate: "today",
-      allowInput: false,
-    });
+  const checkin = flatpickr("#checkin", {
+    dateFormat: "Y-m-d",
+    minDate: "today",
+    allowInput: false,
+    onChange: function (selectedDates, dateStr, instance) {
+      // Update checkout's minDate when a checkin date is selected
+      if (selectedDates.length > 0) {
+        checkout.set("minDate", selectedDates[0]);
+      }
+    },
   });
+
+  const checkout = flatpickr("#checkout", {
+    dateFormat: "Y-m-d",
+    minDate: "today",
+    allowInput: false,
+  });
+});
